0ea749fffc5b4d118f315b6f2a1a14cb010380d3
XxDoy <69180906+XxDoy@users.noreply.github.com>
2021-01-20 21:14:11 +0700

    Update verify.js

--- commands/utilities/verify.js
+++ commands/utilities/verify.js
@@ -5,18 +5,6 @@ module.exports = {
     category: "utilites",
     cooldown: 20,
     run: async(client, message, args) => {
-        let user;
-
-    if (message.mentions.users.first()) {
-        user = message.mentions.users.first();
-    } else if (args[0]) {
-        user = message.guild.members.cache.get(args[0]).user;
-    } else {
-        user = message.author;
-    }
-
-        let avatar = user.displayAvatarURL({ size: 3096, dynamic: true });
-
         if (message.channel.id !== "798808092426240021") {
             return;
         }
@@ -26,8 +14,7 @@ module.exports = {
     let embed = new MessageEmbed()
             .setColor('BLACK')
             .setTitle(`${message.author.username}`)
-            .setDescription('**Verified** <a:checklist:801435724636880936>\n\n```EXAMPLE : h?verify```')
-            .setThumbnail(avatar)
+            .setDescription('**Successfully Verified!!** <a:checklist:801435724636880936>\n\n```EXAMPLE : h?verify```')
             .setTimestamp()
         await message.channel.send(embed)
         msg.delete()
