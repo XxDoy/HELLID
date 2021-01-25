50fa3e69f187855afb92c863b997feafc5fe024f
XxDoy <69180906+XxDoy@users.noreply.github.com>
2021-01-25 12:55:27 +0700

    Create dm.js

---
+++ commands/moderation/dm.js
@@ -0,0 +1,83 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: "msg",
+    category: "moderation",
+    run: async (client, message, args) => {
+
+
+        let user;
+
+        if (message.mentions.users.first()) {
+            user = message.mentions.users.first();
+        } else if (args[0]) {
+            user = message.guild.members.cache.get(args[0]).user;
+        } else {
+            user = message.author;
+        }
+
+        let avatar = user.displayAvatarURL({ size: 4096, dynamic: true });
+
+
+    let rMember = message.mentions.members.first() || message.guild.members.get(args[0]);
+
+    let textChannel = message.mentions.channels.first()
+
+    let tertuju = args[1]
+    let angka = args[2]
+
+    let msgs = args.slice(1).join(" ");
+
+    if (!rMember) return message.reply("Couldn't find that person").then(m => m.delete({ timeout: 5000 }));
+
+    if (!textChannel) return message.channel.send(`Tag Channel Yang Bener Kasian Di DM Invalid :)`)
+
+    if (!tertuju | !Number(angka)) {
+        return message.channel.send(`
+        'hrc!send (user) (pesan)
+        **EXAMPLE:**
+        h?msg @Yaza#9481 Apa Kabar?'`).then(m => m.delete({ timeout: 5000 }));
+    }
+
+
+    const mgs = new MessageEmbed()
+        .setDescription(`Requested By ${message.author.username}.......`)
+
+    const msg1 = new MessageEmbed()
+        .setDescription("**Loading**")
+
+    const msg2 = new MessageEmbed()
+        .setDescription("<a:at_loading:782088166470451241>")
+
+    const msg3 = new MessageEmbed()
+        .setDescription("<a:at_loading:782088166470451241>")
+
+    const msg4 = new MessageEmbed()
+        .setDescription("Done <a:at_verifblack:781014376978055188>")
+
+    const msg5 = new MessageEmbed()
+        .setDescription(`<a:at_verifblack:781014376978055188> | ***${rMember} Done, Check Your Dm!!***`)
+
+    const embed = new MessageEmbed()
+        .setTitle(`${message.guild.name}`)
+        .setThumbnail(avatar)
+        .setColor("RED")
+        .addFields({
+            name: `MESSAGE`,
+            value: `${msgs}`,
+            inline: false
+        })
+        .setTimestamp()
+        .setFooter(`FROM : ${message.author.username}`)
+
+    if (rMember) {
+        var Msg1 = await message.channel.send(mgs); // sends message
+        Msg1.edit(msg1)
+        Msg1.edit(msg2)
+        Msg1.edit(msg3)
+        Msg1.edit(msg4)
+        Msg1.edit(msg5).then(() => rMember.send(embed))
+    }
+
+}
+}
