9732d460b454f1666ff940842788efdbaef546aa
XxDoy <69180906+XxDoy@users.noreply.github.com>
2021-01-27 10:45:17 +0700

    Update snipe.js

--- commands/utilities/snipe.js
+++ commands/utilities/snipe.js
@@ -1,25 +1,21 @@
-const { Discord, MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require('discord.js')
+const db = require("quick.db");
 
 module.exports = {
     name: "snipe",
     category: "utilites",
     run: async(client, message, args) => {
 
-        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-
-        const avatar = message.author.displayAvatarURL({ size: 4096, dynamic: true });
-
-        const msg = client.snipes.cache.get(message.channel.id);
-        if (!msg) return message.reply("no recently deleted messages!");
-
+        message.delete(5000)
+        const msg = client.snipes.get(message.channel.id)
+        if (!msg) return message.channel.send("no recently deleted messages!")
         const embed = new MessageEmbed()
-        .setAuthor(msg.author, avatar)
-        .setDescript(msg.content)
-        .setFooter('Get Snipe')
-        .setTimestamp();
+            .setAuthor(msg.author)
+            .setColor("Random")
+            .setDescription(msg.content)
+            .setTimestamp()
+        if (msg.image) embed.setImage(msg.image)
 
-        if (msg.image) embed.setImage(msg.image);
-
-        message.channel.send(embed);
+        message.channel.send(embed)
     }
 }
