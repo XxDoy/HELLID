228eba2fb9c8e766b0069cf220c8e1008b923785
XxDoy <69180906+XxDoy@users.noreply.github.com>
2021-01-27 11:17:39 +0700

    Update snipe.js

--- commands/utilities/snipe.js
+++ commands/utilities/snipe.js
@@ -6,7 +6,6 @@ module.exports = {
     category: "utilites",
     run: async(client, message, args) => {
 
-        message.delete(5000)
         const msg = client.snipes.get(message.channel.id)
         if (!msg) return message.channel.send("no recently deleted messages!")
         const embed = new MessageEmbed()
