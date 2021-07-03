986a12b39d2e122742259b14f7896b084e0bb447
XxDoy <69180906+XxDoy@users.noreply.github.com>
2021-07-03 13:02:02 +0700

    a

--- commands/utilities/invite.js
+++ commands/utilities/invite.js
@@ -5,12 +5,14 @@ module.exports = {
     category: "utilites",
     timeout: 10000,
     run: async(client, message, args) => {
+
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
     
-    const kiss = new MessageEmbed()
+    const embed = new MessageEmbed()
         .setColor('RANDOM')
-        .setThumbnail('https://media.discordapp.net/attachments/798569406149820426/798647266776055828/20210113_031900.jpg?width=1176&height=676')
+        .setThumbnail(client.user.displayAvatarURL())
         .setDescription(`Want to add ${client.user.username} to your Server?\n\n\n[Click Here](https://discord.com/api/oauth2/authorize?client_id=775207976318664714&permissions=8&scope=bot)`)
-    return message.channel.send(kiss)
+    return message.channel.send(embed)
 
 }
 }
